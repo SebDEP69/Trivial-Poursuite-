cf4772a19969b228e073d0e5113ae5f77c57f076
Anthoo69 <31857119+Anthoo69@users.noreply.github.com>
2017-10-18 16:11:07 +0200

    Mystere Prochain Camembert

--- src/Model/MystereProchainCamembert.java
+++ src/Model/MystereProchainCamembert.java
@@ -13,6 +13,22 @@ public class MystereProchainCamembert extends Mystere{
 	@Override
 	void Action(Joueur joueurcourant, ArrayList<Case> listecase, ArrayList<Joueur> listejoueur) {
 		
+		boolean trouveCaseCamembert = false;
+		Case caseJoueur = joueurcourant.getCaseCourant();
+		int i = caseJoueur.getNumero();
+		
+		while(trouveCaseCamembert != true && i < listecase.size()){
+			if(listecase.get(i).isSuperCamembert()){
+				caseJoueur.setNumero(i);
+				trouveCaseCamembert = true;
+				
+			}
+			else{
+				trouveCaseCamembert = false;
+			}
+			i++;
+		}
+		
 	}
 
 }
