b3203539b83f029bee0ff1e642c20afbaae0b7bd
Mamene <1696185+ben-best@hotmail.fr>
2017-12-28 13:44:49 +0100

    Vérif ajout question

--- src/Controler/ControleurAccueil.java
+++ src/Controler/ControleurAccueil.java
@@ -56,13 +56,13 @@ public class ControleurAccueil  extends Observable{
 	public void creerMenu() {
 
 		Menu menu = new Menu("Trivial Pursuit",this);
-		
+
 		menu.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		menu.setVisible(true);
 		menu.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		this.addObserver((Observer) menu);
 		this.afficherMenu();
-		
+
 
 	}
 	public void afficherMenu() {
@@ -87,46 +87,83 @@ public class ControleurAccueil  extends Observable{
 
 	@SuppressWarnings("static-access")
 	public void enregisterQuestion(String typeQuestion,String question,String[] reponses,String numReponseJuste,String descritption) {
-
-		System.out.println(typeQuestion);
-		System.out.println(question);
-		System.out.println(reponses[0]);
-		System.out.println(reponses[1]);
-		System.out.println(reponses[2]);
-		System.out.println(reponses[3]);
-		System.out.println(numReponseJuste);
-		System.out.println(descritption);
-		BaseQuestionCSV BDD = new BaseQuestionCSV();
-
-		Couleur color= Couleur.ORANGE;
-		switch (typeQuestion) {
-		case "CPE":
-			color= Couleur.VERT;
-			break;
-		case "Blague":
-			color= Couleur.ORANGE;
-			break;
-		case "Innovation":
-			color= Couleur.ROUGE;
-			break;
-		case "Le saviez-vous?":
-			color= Couleur.BLEU;
-			break;
-		default:
-			color= Couleur.ORANGE;
-			break;
-		}
-
-		try {
-			BDD.ajouterQuestion(color, question, reponses, numReponseJuste, descritption);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		JOptionPane jop1;
 		//Boîte du message d'information
-		jop1 = new JOptionPane();
-		jop1.showMessageDialog(null, "Votre question a bien été ajoutée", "Ajout de la question", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane jop1 = new JOptionPane();
+		String textPbl ="<html>";
+		boolean error= false;
+		if (typeQuestion.equals("")) {
+			error= true;
+			textPbl =textPbl+"<br>- Veuillez selectioner un type de question";
+		}
+		if (question.equals("")) {
+			error= true;
+			textPbl =textPbl+"<br>- Veuillez entrer une question";
+		}
+		if (reponses[0].equals("")) {
+			error= true;
+			textPbl =textPbl+"<br>- Veuillez entrez la réponse 1";
+		}
+		if (reponses[1].equals("")) {
+			error= true;
+			textPbl =textPbl+"<br>- Veuillez entrez la réponse 2";
+		}
+		if (reponses[2].equals("")) {
+			error= true;
+			textPbl =textPbl+"<br>- Veuillez entrez la réponse 3";
+		}
+		if (reponses[3].equals("")) {
+			error= true;
+			textPbl =textPbl+"<br>- Veuillez entrez la réponse 4";
+		}
+		if (descritption.equals("")) {
+			error= true;
+			textPbl =textPbl+"<br>- Veuillez entrer une description";
+		}
+		textPbl =textPbl+"</html>";
+
+		if (error) {
+			jop1.showMessageDialog(null, textPbl, "Erreur champ manquant", JOptionPane.ERROR_MESSAGE);
+
+		}else{
+			/*System.out.println(typeQuestion);
+			System.out.println(question);
+			System.out.println(reponses[0]);
+			System.out.println(reponses[1]);
+			System.out.println(reponses[2]);
+			System.out.println(reponses[3]);
+			System.out.println(numReponseJuste);
+			System.out.println(descritption);*/
+			BaseQuestionCSV BDD = new BaseQuestionCSV();
+
+			Couleur color= Couleur.ORANGE;
+			switch (typeQuestion) {
+			case "CPE":
+				color= Couleur.VERT;
+				break;
+			case "Blague":
+				color= Couleur.ORANGE;
+				break;
+			case "Innovation":
+				color= Couleur.ROUGE;
+				break;
+			case "Le saviez-vous?":
+				color= Couleur.BLEU;
+				break;
+			default:
+				color= Couleur.ORANGE;
+				break;
+			}
+
+			try {
+				BDD.ajouterQuestion(color, question, reponses, numReponseJuste, descritption);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+
+
+			jop1.showMessageDialog(null, "<html>Votre question a bien été ajoutée <br> coucou</html>", "Ajout de la question", JOptionPane.INFORMATION_MESSAGE);
+		}
 	}
 
 
@@ -143,7 +180,7 @@ public class ControleurAccueil  extends Observable{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 
 		this.notifyObservers(info);
 
@@ -152,7 +189,7 @@ public class ControleurAccueil  extends Observable{
 
 	public void AfficherdetailPartie(Partie partie){
 
-	
+
 		ArrayList<Object> info = new ArrayList<Object>();
 		info.add(3);
 		info.add(partie);
